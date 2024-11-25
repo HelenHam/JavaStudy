36982e2d8846ea093ab00b0f434ed1c36d441f12
HelenHam <hh0802@naver.com>
2024-11-25 13:18:23 +0900

    생성자

---
+++ src/Study16.java
@@ -0,0 +1,13 @@
+public class Study16 {
+
+    public static void main(String[] args) {
+
+        // Study16_A s16A = new Study16_A();
+        // s16A.run();
+        // 을 줄이면
+
+        new Study16_A().run();
+    }
+
+    
+}
